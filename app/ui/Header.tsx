6797c5e746c8cd038084293faf464d90d31c088c
greenyellowlight <greenyellowlight@protonmail.com>
2026-07-20 20:37:17 +1000

    homepage button

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,6 +3,12 @@ export default function Header() {
     return (
         <header className="w-full bg-green-900  px-6 py-16 shadow-lg">
             <div className="max-w-3xl mx-auto px-4">
+                <a
+                    href="/"
+                    className="inline-block mb-4 text-green-100  hover:text-white transition-colors"
+                >
+                    ← Homepage
+                </a>
                 <h1 className="text-5xl font-bold text-white mb-4">Garden Planner</h1>
                 <p className="text-green-100 text-lg max-w-xl leading-relaxed">
                     See how your garden would look with more plants
